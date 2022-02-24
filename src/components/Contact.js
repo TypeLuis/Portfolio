0070b0d39741c2e0e06d709740bee4f853790602
typeluis <typeluis@gmail.com>
2022-02-24 00:04:48 -0500

    added icon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,42 +1,60 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import emailjs from '@emailjs/browser';
+import env from 'react-dotenv'
 
 const Contact = (props) => {
-    const [email, setEmail] = useState('')
+    const [responseStatus, setResponseStatus] = useState('')
     const [name, setName] = useState('')
-    const [message, setMessage] = useState('')
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const response = await emailjs.sendForm('service_f56mnkf', 'template_wew05jt', e.target, 'user_tUz3TSgEw0qE3Ag3DhlbQ');
-        console.log(response)
+        const response = await emailjs.sendForm('service_f56mnkf', 'template_wew05jt', e.target, env.emailjsUserID);
+        setResponseStatus(response.status)
         e.target.reset()
     }
     return (
         <div ref={props.contactRef} id='Contact' className='content-page'>
             <h1 id='Contact-Heading' className='heading'>Contact</h1>
 
-            {/* <form action='https://formsubmit.co/typeluis@gmail.com' method='POST'> */}
-            <form onSubmit={(e) => { handleSubmit(e) }}>
+            {responseStatus === 200 ?
 
-                <div className='textbox email'>
-                    <label htmlFor="email"></label>
-                    <input type="email" name="email" placeholder="Email Address" required />
+
+                <div>
+
+                    <h1>
+                        Thank you for your consideration {name}! I will be sure to get In contact as soon as possible!
+                    </h1>
+
                 </div>
 
-                <div className='textbox name'>
-                    <label htmlFor="name"></label>
-                    <input type="text" name='name' placeholder='Full Name' required />
-                </div>
 
-                <div className='textbox message'>
-                    <label htmlFor="message"></label>
-                    <input type="text" name='message' placeholder='Message' required />
-                </div>
 
-                <button type='submit'>Send</button>
-            </form>
+                :
+
+
+
+                <form onSubmit={(e) => { handleSubmit(e) }}>
+
+                    <div className='textbox email'>
+                        <label htmlFor="email"></label>
+                        <input type="email" name="email" placeholder="Email Address" required />
+                    </div>
+
+                    <div className='textbox name'>
+                        <label htmlFor="name"></label>
+                        <input type="text" name='name' placeholder='Full Name' required onChange={(e) => setName(e.target.value)} />
+                    </div>
+
+                    <div className='textbox message'>
+                        <label htmlFor="message"></label>
+                        <input type="text" name='message' placeholder='Message' required />
+                    </div>
+
+                    <button type='submit'>Send</button>
+                </form>
+
+            }
 
         </div>
     )
